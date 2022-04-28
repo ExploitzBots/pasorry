4641534a9d70f691935c351c05770a4d099793ed
EvilzBots <85481867+EvilzBots@users.noreply.github.com>
2022-04-28 08:12:32 +0530

    Update __main__.py

--- PyroBot/__main__.py
+++ PyroBot/__main__.py
@@ -7,15 +7,15 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # vars
-APP_ID = config("APP_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
+APP_ID = "5689646"
+API_HASH = "895de5ae804308803c19814afabb0de7"
+BOT_TOKEN = "5205589679:AAFlYLOb1TJ0uihGnCgWwASf6_8_i4ClEP0"
 
 if __name__ == "__main__" :
     print("Starting Bot...")
     plugins = dict(root="PyroBot/plugins")
     app = pyrogram.Client(
-        "BotzHub",
+        "Demon",
         bot_token=BOT_TOKEN,
         api_id=APP_ID,
         api_hash=API_HASH,
