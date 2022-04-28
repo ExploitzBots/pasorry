a100eb11ddf3be3eed51ba02ab92eb7d4ff39d82
ExploitzBots <79147710+ExploitzBots@users.noreply.github.com>
2022-04-28 08:54:42 +0530

    Update start.py

--- PyroBot/plugins/start.py
+++ PyroBot/plugins/start.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
 
 @Client.on_message(filters.command(["start"]))
-async def start(bot, update):
-    await bot.send_message("Hi")
+async def start(bot, message):
+    await message.reply_text(text="Hy",reply_to_message_id=message.message_id)
